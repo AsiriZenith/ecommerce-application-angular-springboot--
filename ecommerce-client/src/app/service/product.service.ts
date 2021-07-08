31935931f4806f95ba912ade7ad10b7646dfa1f9
AsiriZenith <asirisenith@gmail.com>
2021-07-08 20:18:05 +0530

    implemented get product category by id

--- ecommerce-client/src/app/service/product.service.ts
+++ ecommerce-client/src/app/service/product.service.ts
@@ -3,13 +3,17 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { map } from 'rxjs/operators';
 import { Product } from "../commen/product";
+import { ProductCategory } from "../commen/product-category";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
+    // full project github Url
+    // github Url : https://github.com/darbyluv2code/fullstack-angular-and-springboot
 
     private baseUrl = 'http://localhost:8088/api/products';
+    private categoryUrl = 'http://localhost:8088/api/product-category';
 
     constructor(
         private httpClient: HttpClient
@@ -17,15 +21,29 @@ export class ProductService {
 
     getProductList(categoryId: number): Observable<Product[]> {
 
-        // @TODO: need to build URL based on category id ... will come back to this! 
-        return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+        // need to build URL based on category id 
+        const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+
+        return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
             map(res => res._embedded.products)
+        );
+    }
+
+    getProductCategories(): Observable<ProductCategory[]> {
+        return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+            map(res => res._embedded.productCategory)
         );
     }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
     _embedded: {
         products: Product[];
+    }
+}
+
+interface GetResponseProductCategory {
+    _embedded: {
+        productCategory: ProductCategory[];
     }
 }
