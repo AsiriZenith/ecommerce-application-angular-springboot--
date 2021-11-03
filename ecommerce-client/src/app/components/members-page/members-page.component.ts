5965937f45384c55feec7471495372303a30b443
AsiriZenith <asirisenith@gmail.com>
2021-11-03 13:30:21 +0530

    ecommerce client changes

--- ecommerce-client/src/app/components/members-page/members-page.component.ts
+++ ecommerce-client/src/app/components/members-page/members-page.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-    selector:'members-page',
-    templateUrl:'./members-page.component.html',
-    styleUrls:['./members-page.component.css']
+    selector: 'members-page',
+    templateUrl: './members-page.component.html',
+    styleUrls: ['./members-page.component.css']
 })
 
 export class MembersPageComponent implements OnInit {
